da2d3d2a3c2fe83f49ba9a14810cdb0a30b29bb4
Martin Dünkelmann <nc-duenkekl3@netcologne.de>
2022-10-20 18:31:57 +0200

    fix(motd): exception counting processes as root
    Signed-off-by: Martin Dünkelmann <nc-duenkekl3@netcologne.de>

--- base/further-steps/examples/motd/update_motd.sh
+++ base/further-steps/examples/motd/update_motd.sh
@@ -24,7 +24,7 @@ fi
 MOTD=$MOTD$WHITE$(uname -a)"\n"
 MOTD=$MOTD$WHITE$(uptime | cut -c2-)"\n"
 MOTD=$MOTD$BLUE_LIGHT"CPU\t\t$WHITE= $(grep -P 'model name\t: ' </proc/cpuinfo | uniq | cut -c14-)\n"
-MOTD=$MOTD$BLUE_LIGHT"Processes\t$WHITE= You are running $(ps U "$USER" h | wc -l) of $(ps -Afl | wc -l) processes \n"
+MOTD=$MOTD$BLUE_LIGHT"Processes\t$WHITE= You are running $(ps -A | wc -l) processes \n"
 MOTD=$MOTD$BLUE_LIGHT"Temperatures\n"
 MOTD=$MOTD"$WHITE$(sensors)\n\n"
 MOTD=$MOTD$BLUE_LIGHT"Memory & Swap\n"
